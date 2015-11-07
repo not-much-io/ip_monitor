2d544119fcc7dff65db2a5f9d7f5e24c50b79ba3
not-much-io <kristo.koert@gmail.com>
2015-11-07 13:59:58 +0200

    Initial commit

---
+++ ip_monitor.py
@@ -0,0 +1,60 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from time import sleep
+from requests import get
+
+conf = dict(
+    # Any email address
+    toEmail='',
+    # Google e-mail address
+    fromEmail='',
+    # Google password
+    password=''
+)
+
+
+class Mailer:
+    def __init__(self, subject, body):
+        self.subject = subject
+        self.body = body
+
+    def send_mail(self):
+        from_addr = conf['fromEmail']
+        to_addr = conf['toEmail']
+        password = conf['password']
+
+        msg = MIMEMultipart()
+        msg['From'] = from_addr
+        msg['To'] = to_addr
+        msg['Subject'] = self.subject
+
+        msg.attach(MIMEText(self.body, 'plain'))
+
+        text = msg.as_string()
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.login(from_addr, password)
+        server.starttls()
+        server.sendmail(from_addr, to_addr, text)
+        server.quit()
+
+
+def get_ip():
+    return get('https://api.ipify.org').text
+
+
+if __name__ == '__main__':
+    last_ip = ""
+    while True:
+        curr_ip = get_ip()
+
+        if curr_ip != last_ip:
+            newMail = Mailer("External IP has changed.",
+                             "New IP: " + curr_ip)
+            newMail.send_mail()
+            last_ip = curr_ip
+            print("Sent new IP: ", curr_ip)
+        else:
+            print("ZZzz..")
+            sleep(3600)
